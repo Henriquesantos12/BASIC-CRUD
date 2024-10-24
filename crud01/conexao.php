2054987f67c0d998a92fcb2aa079051d2b4dadee
Henrique Santos <114838533+Henriquesantos12@users.noreply.github.com>
2024-10-24 17:36:50 -0300

    arquivo de conexão feito

---
+++ crud01/conexao.php
@@ -0,0 +1,12 @@
+  <?
+    $server = "localhost";
+    $user = "root";
+    $pass = "";
+    $bd = "usuario";
+
+    if ($conn = mysqli_connect($server, $user, $pass, $bd)) {
+s    } else {
+        echo "Erro";
+    }
+
+    ?>
